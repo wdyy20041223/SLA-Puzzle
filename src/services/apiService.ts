e8ded84d8b895e6a5581c4de7110f0dedd939a6d
invain01 <3038026071@qq.com>
2025-09-09 10:19:05 +0800

    fix.本地api修改

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -60,8 +60,8 @@ class ApiService {
 
   constructor() {
     // 从环境变量或配置文件获取API基础URL
-    this.baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3001/api';
-    
+    //this.baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3001/api';
+    this.baseURL = import.meta.env.VITE_API_BASE_URL || 'http://10.194.112.121/api';
     // 从localStorage获取保存的token
     this.token = localStorage.getItem('puzzle_auth_token');
   }
